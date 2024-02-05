e1b7ab1f3d85d83647e56cb7da6835e783a72160
sergejmihajlov <smikhaylov_2011@mail.ru>
2024-02-05 16:49:19 +0300

    Start worked on import las-file.

---
+++ petrocalculator/las_files/import_las.py
@@ -0,0 +1,6 @@
+import lasio
+
+# в переменной filename указывается путь к las-файлу
+filename = '/Users/sergejmihajlov/Documents/Обучающая база/Gulfaks/Input/Wells/A10.las'
+
+las = lasio.read(filename)
